800bf01cdecdb1e1b61b760176a264f58c782250
Abel Kelemework <81910594+Abelk-sys@users.noreply.github.com>
2024-06-10 22:40:56 +0300

    Add files via upload

---
+++ portfolio#2 - Copy/script.js
@@ -0,0 +1,55 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+
+            })
+        }
+    })
+}
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+function updateProgressBar(id, percentage) {
+    var progressBar = document.getElementById(id);
+    progressBar.style.width = percentage + '%';
+    progressBar.textContent = percentage + '%';
+  }
+  
+  // Example usage: Update the progress bars to different values after 1 second
+  setTimeout(function() {
+    updateProgressBar('progress-bar1', 80);
+    updateProgressBar('progress-bar2', 70);
+    updateProgressBar('progress-bar3', 70);
+    updateProgressBar('progress-bar4', 90);
+    updateProgressBar('progress-bar5', 60);
+    updateProgressBar('progress-bar6', 100);
+    
+  }, 1000);
+
+  function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        number : document.getElementById("number").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+    emailjs.send("service_dv7ala3","template_a0gw3z8",parms).then(alert("Email Sent !!!"))
+  }
+ 
